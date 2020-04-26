612c52c51935697cbfd8b3de214fa3da1a19ca96
Borsuk <borsuk@macbook-pro-macbook.home>
2020-04-26 20:50:00 +0200

    Updates

---
+++ precache-manifest.7de72124f242837b663d61530d7c1400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2b1ae7608f90fdb9b594cbf21e8738",
+    "url": "/user-app/index.html"
+  },
+  {
+    "revision": "b4e49e6027fb4254d775",
+    "url": "/user-app/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "bf2a373a9f67b34febf3",
+    "url": "/user-app/static/css/main.b91d6018.chunk.css"
+  },
+  {
+    "revision": "b4e49e6027fb4254d775",
+    "url": "/user-app/static/js/2.5eb0a95a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/user-app/static/js/2.5eb0a95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2a373a9f67b34febf3",
+    "url": "/user-app/static/js/main.19828c11.chunk.js"
+  },
+  {
+    "revision": "5b0d98dea781994e8416",
+    "url": "/user-app/static/js/runtime-main.e00af3ea.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/user-app/precache-manifest.7de72124f242837b663d61530d7c1400.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/user-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
